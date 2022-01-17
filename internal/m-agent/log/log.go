d62f7249ea9e3c12a5a553c69866baf16e3b033c
neelanjan00 <neelanjan@chaosnative.com>
2022-01-17 13:16:42 +0530

    added log comments
    Signed-off-by: neelanjan00 <neelanjan@chaosnative.com>

--- internal/m-agent/log/log.go
+++ internal/m-agent/log/log.go
@@ -7,31 +7,37 @@ import (
 	"github.com/litmuschaos/m-agent/internal/m-agent/errorcodes"
 )
 
+// GetTokenErrorLogger returns a logger for handling token errors
 func GetTokenErrorLogger() *log.Logger {
 
 	return log.New(os.Stdout, errorcodes.GetTokenErrorPrefix(), log.Ldate|log.Ltime|log.Lmsgprefix)
 }
 
+// GetClientMessageReadErrorLogger returns a logger for handling client message read errors
 func GetClientMessageReadErrorLogger() *log.Logger {
 
 	return log.New(os.Stdout, errorcodes.GetClientMessageReadErrorPrefix(), log.Ldate|log.Ltime|log.Lmsgprefix)
 }
 
+// GetSteadyStateCheckErrorLogger returns a logger for handling steady state check errors
 func GetSteadyStateCheckErrorLogger() *log.Logger {
 
 	return log.New(os.Stdout, errorcodes.GetSteadyStateCheckErrorPrefix(), log.Ldate|log.Ltime|log.Lmsgprefix)
 }
 
+// GetExecuteExperimentErrorLogger returns a logger for handling execute experiment errors
 func GetExecuteExperimentErrorLogger() *log.Logger {
 
 	return log.New(os.Stdout, errorcodes.GetExecuteExperimentErrorPrefix(), log.Ldate|log.Ltime|log.Lmsgprefix)
 }
 
+// GetCommandProbeExecutionErrorLogger returns a logger for handling command probe execution errors
 func GetCommandProbeExecutionErrorLogger() *log.Logger {
 
 	return log.New(os.Stdout, errorcodes.GetCommandProbeExecutionErrorPrefix(), log.Ldate|log.Ltime|log.Lmsgprefix)
 }
 
+// GetInvalidActionErrorLogger returns a logger for handling invalid action errors
 func GetInvalidActionErrorLogger() *log.Logger {
 
 	return log.New(os.Stdout, errorcodes.GetInvalidActionErrorPrefix(), log.Ldate|log.Ltime|log.Lmsgprefix)
